6960e3bc45da7531a93f00a01e18ce98cd18dde4
saifuddien <ariefsaifuddien01@gmail.com>
2022-05-29 16:32:57 +0200

    plp

--- routers/verify.js
+++ routers/verify.js
@@ -13,9 +13,7 @@ export const verifyToken = (req, res, next) => {
   }
 
   try {
-    const verified = jsonwebtoken.verify(token, process.env.SECRET_KEY)
-    req.auth = verified
-    console.log(req.auth)
+    jsonwebtoken.verify(token, process.env.SECRET_KEY)
     next()
   } catch (error) {
     return res.status(400).json({
