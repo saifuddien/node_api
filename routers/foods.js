11d5ab0ff29712b519420f91e456e74917765870
saifuddien <ariefsaifuddien01@gmail.com>
2022-05-31 13:44:34 +0200

    o

--- routers/foods.js
+++ routers/foods.js
@@ -5,7 +5,7 @@ import { verifyToken } from "./verify.js";
 export const foodsRouter = express.Router()
 
 foodsRouter.post('/foods', verifyToken, addFoods)
-foodsRouter.get('/foods', verifyToken, getFoods)
+foodsRouter.get('/foods', getFoods)
 foodsRouter.get('/foods/:id', getOneFoods)
 foodsRouter.put('/foods/:id', updateOneFoods)
 foodsRouter.delete('/foods/:id', deleteFoods)
